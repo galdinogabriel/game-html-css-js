31ce2454240fce285ff2980ca7d96d1737cb4015
Gabriel Matos <matosgabriel017@gmail.com>
2023-02-14 22:22:03 -0300

    mais rapido

--- js/flappy.js
+++ js/flappy.js
@@ -58,7 +58,7 @@ function Barreiras(altura, largura, abertura, espaco, notificarPonto) {
     new parDeBarreiras(altura, abertura, largura + espaco * 3),
   ];
 
-  const deslocamento = 2;
+  const deslocamento = 3;
   this.animar = () => {
     this.pares.forEach((par) => {
       par.setX(par.getX() - deslocamento);
